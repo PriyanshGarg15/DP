0dd07a9f01af07b053964f491d781c4840386e34
priyanshgarg15 <priyanshgargagra123@gmail.com>
2024-07-18 23:46:01 +0530

    first commit

--- a1_fibonacci_using_top_down_memoization_approach.java
+++ a1_fibonacci_using_top_down_memoization_approach.java
@@ -7,7 +7,7 @@ public class a1_fibonacci_using_top_down_memoization_approach {
         int n = sc.nextInt();
         
         int dp[] = new int[n + 1];
-        
+        Arrays.fill(dp,-1);
         // Using memoization (top-down approach)
         int resultMemo = fdp(n, dp);
         System.out.println("Fibonacci number using memoization: " + resultMemo);
@@ -18,7 +18,7 @@ public class a1_fibonacci_using_top_down_memoization_approach {
         if (n == 0 || n == 1) {
             return n;
         }
-        if (dp[n] != 0) {
+        if (dp[n] != -1) {
             return dp[n];
         }
         int n1 = fdp(n - 1, dp);
